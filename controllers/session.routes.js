3870a20968bfc1d6baf25733476adb8bcd020eb1
doodybh <doodybhr38@gmail.com>
2026-05-13 06:36:19 +0300

    also forgot to include to that some minor bugs regarding the put route were fixed for glasses and other cosmetics to function with the coin system

--- controllers/session.routes.js
+++ controllers/session.routes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const User = require("../models/User");
 const Subject = require("../models/Subject");
 
-// COMPLETE STUDY SESSION
 router.post("/complete", async (req, res) => {
   try {
     const { studiedMinutes, breakMinutes, subjectId } = req.body;
